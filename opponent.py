21bb988bb78f7a3f05d281d4b314b57f62101fc1
eebop <kieran.bieniosek@gmail.com>
2021-01-26 15:11:17 -0800

    removed select in oppennent.py TEST

--- opponent.py
+++ opponent.py
@@ -1,7 +1,7 @@
 from player import player
 from base import request
 import numpy as np
-from select import select
+#from select import select
 
 class opponent(player):
     def __init__(self):
@@ -10,6 +10,6 @@ class opponent(player):
     def update(self, time, data):
         self.target = self.loc + np.array(self.image_now.get_size())/2
         double_socket = request().double_socket
-        if select([double_socket.reciver], [], [], 0)[0]:
-            self.move(double_socket.recive(False))
+        #if select([double_socket.reciver], [], [], 0)[0]:
+        self.move(double_socket.recive(False))
         return self.image_now, list(self.loc)
