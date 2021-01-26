062e9da5e3614a3b78b9a2020db2cbe308c3c170
eebop <kieran.bieniosek@gmail.com>
2021-01-26 15:32:05 -0800

    Optimized lan/guest.py

--- lan/guest.py
+++ lan/guest.py
@@ -1,7 +1,4 @@
-import framework
-import norm_keys_wrapper
 import sys
-import time
 import pygame
 from select import select
 
@@ -13,15 +10,19 @@ class guest:
 
     def run(self):
         while True:
-            self.double_socket.send(self._process_events(self.events))
+            e = self._process_events(self.events)
+            if e:
+                self.double_socket.send(e)
             if select([self.double_socket.reciver], [], [], 0)[0]:
                 self.screen.blit(self.double_socket.recive(), (0, 0))
             pygame.display.flip()
             self.events = pygame.event.get()
 
 
-    def _process_events(self, events):
-        events = [self.eventtoeventwrapper(e) for e in events if e.type in (pygame.KEYDOWN, pygame.KEYUP)]
+    def _process_events(self):
+        events = pygame.event.get()
+        sys.exit() if any([event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_c and event.mod == pygame.KMOD_LCTRL) for event in e]) else None
+        events = [self.eventtoeventwrapper(e) for e in  if e.type in (pygame.KEYDOWN, pygame.KEYUP)]
         return events
 
     def eventtoeventwrapper(self, event):
