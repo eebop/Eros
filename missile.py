cb242bc4da91e074e80c02fc72debd6af021efb6
eebop <kieran.bieniosek@gmail.com>
2021-02-05 17:18:25 -0800

    Updated missile.py

--- missile.py
+++ missile.py
@@ -12,10 +12,15 @@ class missile:
         self.goal = np.array([500, 500], dtype=float)
         self.frozen = False
         self.IsLaunchedByPlayer = islaunchedbyplayer
+        self.movement = (0, 0)
 
     def update(self, time, data):
         self.get_goal()
         # must return an image to draw and a location to draw at
+        if self.goal = None:
+            self.movement /= 2
+            self._loc += self.movement
+            return self.s, self._loc
         if (self._loc != self.goal).any():
             slope = np.array(self.goal - self._loc, dtype=int)
 
@@ -23,13 +28,13 @@ class missile:
                 radians = math.atan2(*reversed(slope))
             else:
                 radians = math.pi/2 if slope[1] > 0 else -math.pi/2
-            self._loc += np.array([math.cos(radians), math.sin(radians)])
+            self.movement += np.array([math.cos(radians), math.sin(radians)])
+            self._loc += self.movement
 
 
             if ([-10, -10] < self.goal-self._loc).all() and (self.goal-self._loc < [10, 10]).all() and not self.frozen:
                 request().extentions['target'].get_target(self.IsLaunchedByPlayer).kill()
                 request()._items.pop(request()._items.index(self))
-                self.loc = self.goal
                 return pygame.Surface((1, 1)), (0, 0)
             return self.s, self._loc
         return self.s, self._loc
@@ -45,4 +50,4 @@ class missile:
 
     def freeze(self):
         self.frozen = True
-        return self._loc
+        return None
